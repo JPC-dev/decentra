d190a03efe806bd5bd70a6c485ee60fd1f9803d7
KGJason <jason13may@gmail.com>
2022-03-23 19:29:59 -0400

    staking function

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,20 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
-  constructor(address exampleExternalContractAddress) public {
+  mapping (address => uint256) public balance;
+  uint256 public constant threshold = 1 ether;
+
+
+  constructor(address exampleExternalContractAddress){
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
-
+  function Stake() public payable {
+    require(msg.value > 0, "Not enough funds");
+    balance[msg.sender] = msg.value + balance[msg.sender];
+      
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
